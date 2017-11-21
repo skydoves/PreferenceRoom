ec97d8bc25ac753eb4b8afabee39e6877edcf9f3
skydoves <skydoves@naver.com>
2017-11-21 22:49:55 +0900

    update component

--- Processor/src/main/java/com/skydoves/processor/PreferenceComponentGenerator.java
+++ Processor/src/main/java/com/skydoves/processor/PreferenceComponentGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 skydoves
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.skydoves.processor;
 
 import android.content.Context;
@@ -16,11 +32,6 @@ import java.util.Map;
 import static javax.lang.model.element.Modifier.PRIVATE;
 import static javax.lang.model.element.Modifier.PUBLIC;
 import static javax.lang.model.element.Modifier.STATIC;
-
-/**
- * Developed by skydoves on 2017-11-21.
- * Copyright (c) 2017 skydoves rights reserved.
- */
 
 public class PreferenceComponentGenerator {
 
@@ -68,9 +79,8 @@ public class PreferenceComponentGenerator {
                 .addModifiers(PRIVATE)
                 .addParameter(ParameterSpec.builder(Context.class, CONSTRUCTOR_CONTEXT).addAnnotation(NonNull.class).build());
 
-        this.annotatedClazz.keyNames.forEach(keyName -> {
-            builder.addStatement("$N = $N.getInstance($N.getApplicationContext())", getEntityInstanceFieldName(keyName), getEntityClazzName(annotatedEntityMap.get(keyName)), CONSTRUCTOR_CONTEXT);
-        });
+        this.annotatedClazz.keyNames.forEach(keyName ->
+            builder.addStatement("$N = $N.getInstance($N.getApplicationContext())", getEntityInstanceFieldName(keyName), getEntityClazzName(annotatedEntityMap.get(keyName)), CONSTRUCTOR_CONTEXT));
 
         return builder.build();
     }
