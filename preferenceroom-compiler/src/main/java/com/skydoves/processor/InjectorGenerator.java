02b905ad52f402756dd72e6d626d36a6c48811ca
skydoves <skydoves@naver.com>
2017-11-24 01:32:58 +0900

    copyright

--- preferenceroom-compiler/src/main/java/com/skydoves/processor/InjectorGenerator.java
+++ preferenceroom-compiler/src/main/java/com/skydoves/processor/InjectorGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 skydoves
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.skydoves.processor;
 
 import android.support.annotation.NonNull;
@@ -12,11 +28,6 @@ import com.squareup.javapoet.TypeSpec;
 import javax.lang.model.element.TypeElement;
 
 import static javax.lang.model.element.Modifier.PUBLIC;
-
-/**
- * Developed by skydoves on 2017-11-23.
- * Copyright (c) 2017 skydoves rights reserved.
- */
 
 public class InjectorGenerator {
 
