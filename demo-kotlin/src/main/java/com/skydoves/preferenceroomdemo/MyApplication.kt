57f7108120183020a4d84d22d471e9818d513f51
skydoves <skydoves@naver.com>
2018-07-25 17:10:43 +0900

    added stetho dependencies

--- demo-kotlin/src/main/java/com/skydoves/preferenceroomdemo/MyApplication.kt
+++ demo-kotlin/src/main/java/com/skydoves/preferenceroomdemo/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.skydoves.preferenceroomdemo
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 
 import com.skydoves.preferenceroomdemo.components.PreferenceComponent_UserProfileComponent
 
@@ -13,6 +14,8 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // initialize Stetho for debugging local data
+        Stetho.initializeWithDefaults(this)
 
         /**
          * initialize instances of preference component and entities.
--- demo/src/main/java/com/skydoves/preferenceroomdemo/MyApplication.java
+++ demo/src/main/java/com/skydoves/preferenceroomdemo/MyApplication.java
@@ -2,6 +2,7 @@ package com.skydoves.preferenceroomdemo;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.skydoves.preferenceroomdemo.components.AppComponent;
 import com.skydoves.preferenceroomdemo.components.PreferenceComponent_AppComponent;
 
@@ -15,6 +16,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // initialize Stetho for debugging local data
+        Stetho.initializeWithDefaults(this);
 
         /**
          * initialize instances of preference component and entities.
